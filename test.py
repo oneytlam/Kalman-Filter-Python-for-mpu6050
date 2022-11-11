bf5bb01b956dff46df9cfc8edfc5813978aceb55
oneytlam <lam.yikting@dhs.sg>
2022-11-12 00:03:57 +0800

    reading data off csvs now work

--- test.py
+++ test.py
@@ -1,28 +1,20 @@
 # importing the module
 import csv 
 # open the file in read mode
-filename = open('./csvs/x1.csv', 'r')
- 
+file = open('./csvs/x1.csv', 'r')
+
 # creating dictreader object
-file = csv.DictReader(filename)
- 
+file = csv.DictReader(file)
+print(file)
+
 # creating empty lists
 accelX = []
-totalprofit = []
-totalunit = []
+accelY = []
+accelZ = []
  
 # iterating over each row and append
 # values to empty list
 for col in file:
     accelX.append(col['accelX'])
-    totalprofit.append(col['moisturizer'])
-    totalunit.append(col['total_units'])
-
-accelX = []
-accelX = data['accelX'].tolist()
-accelY = data['accelY'].tolist()
-accelZ = data['accelZ'].tolist()
-gyroX = data['accelX'].tolist()
-gyroY = data['accelY'].tolist()
-gyroZ = data['accelZ'].tolist()
-print(accelX)
+    accelY.append(col['accelY'])
+    accelZ.append(col['accelZ'])
