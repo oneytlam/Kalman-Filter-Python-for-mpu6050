d5673f9c9b4cc83c3eb8666220cdb7a7cf52819e
oneytlam <lam.yikting@dhs.sg>
2022-11-11 14:05:11 +0800

    stuck with keyerror

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
-from pandas import *
-data = read_csv("./csvs/x1.csv")
+# importing the module
+import csv 
+# open the file in read mode
+filename = open('./csvs/x1.csv', 'r')
+ 
+# creating dictreader object
+file = csv.DictReader(filename)
+ 
+# creating empty lists
+accelX = []
+totalprofit = []
+totalunit = []
+ 
+# iterating over each row and append
+# values to empty list
+for col in file:
+    accelX.append(col['accelX'])
+    totalprofit.append(col['moisturizer'])
+    totalunit.append(col['total_units'])
+
+accelX = []
 accelX = data['accelX'].tolist()
 accelY = data['accelY'].tolist()
 accelZ = data['accelZ'].tolist()
