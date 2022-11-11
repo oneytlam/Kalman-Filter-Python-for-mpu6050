81542a73dbda4c5ece547bfda3c77839de17130a
oneytlam <lam.yikting@dhs.sg>
2022-11-11 14:01:04 +0800

    more theoretical coding b4 my com dies

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-import csv
-
+from pandas import *
 data = read_csv("./csvs/x1.csv")
 accelX = data['accelX'].tolist()
+accelY = data['accelY'].tolist()
+accelZ = data['accelZ'].tolist()
+gyroX = data['accelX'].tolist()
+gyroY = data['accelY'].tolist()
+gyroZ = data['accelZ'].tolist()
+print(accelX)
