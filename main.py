3d37137eed6f17929bd7e5e682606dc5ff10eb98
TheRoundRoads <michaeljosephgoh@gmail.com>
2022-11-23 09:58:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 import csv
 from pandas import *
 
+timeStamp = []
 accelDataX = []
 accelDataY = []
 accelDataZ = []
@@ -15,6 +16,12 @@ accZ = 0
 gyroX = 0
 gyroY = 0
 gyroZ = 0
+angleX = 0
+angleZ = 0
+velocityX = 0
+velocityZ = 0
+x_coordinate = 0
+z_coordinate = 0
 
 # open the file in read mode
 file = open('./csvs/14anov.csv', 'r')
@@ -22,6 +29,7 @@ file = open('./csvs/14anov.csv', 'r')
 file = csv.DictReader(file)
 
 for col in file:
+	timeStamp.append(col["timestamp"])
 	if (col['accelX'] != None and col['accelY']!=None and col['accelZ']!=None and col['gyroX']!=None and col['gyroY']!=None and col['gyroZ']!=None):
 		accelDataX.append(float(col['accelX']))
 		accelDataY.append(float(col['accelY']))
@@ -29,9 +37,45 @@ for col in file:
 		gyroDataX.append(float(col['gyroX']))
 		gyroDataY.append(float(col['gyroY']))
 		gyroDataZ.append(float(col['gyroZ']))
+    #Assuming that if data is recorded, x, y and z values are present
+	if (col['accelX'] == None and col['gyroX'] == None):
+		accelDataX.append(float(0))
+		accelDataY.append(float(0))
+		accelDataZ.append(float(0))
+		gyroDataX.append(float(0))
+		gyroDataY.append(float(0))
+		gyroDataZ.append(float(0))
+	if (col['accelX'] != None and col['gyroX'] == None):
+		accelDataX.append(float(col['accelX']))
+		accelDataY.append(float(col['accelY']))
+		accelDataZ.append(float(col['accelZ']))
+		gyroDataX.append(float(0))
+		gyroDataY.append(float(0))
+		gyroDataZ.append(float(0))
+	if (col['accelX'] == None and col['gyroX'] != None):
+		accelDataX.append(float(0))
+		accelDataY.append(float(0))
+		accelDataZ.append(float(0))
+		gyroDataX.append(float(col['gyroX']))
+		gyroDataY.append(float(col['gyroY']))
+		gyroDataZ.append(float(col['gyroZ']))
 
-accX = accelDataX[0]
-accY = accelDataY[0]
-accZ = accelDataZ[0]
+prevTime = math.floor(float(timeStamp[0]))
+curTime = 0
 
-print(accelDataX)
+for i in range(10):
+    angleX += gyroDataX[i]
+    angleX %= (2*math.pi)
+    angleZ += gyroDataZ[i]
+    angleZ %= (2*math.pi)
+    accX = accelDataX[i] * math.cos(angleX) + accelDataX[i] * math.sin(angleZ)
+    accZ = accelDataZ[i] * math.sin(angleZ) + accelDataZ[i] * math.cos(angleZ)
+    #assume uniform acceleration
+    curTime = float(timeStamp[i])
+    x_coordinate += velocityX * (curTime - prevTime) + 0.5 * accX * ((curTime - prevTime) ** 2)
+    z_coordinate += velocityZ * (curTime - prevTime) + 0.5 * accZ * ((curTime - prevTime) ** 2)
+    velocityX += accX * (curTime - prevTime)
+    velocityZ += accZ * (curTime - prevTime)
+    print("vx:", velocityX)
+    print("vz:", velocityZ)
+    # print(f"time: {curTime}, x coordinate: {x_coordinate}, z coordinate: {z_coordinate}")
